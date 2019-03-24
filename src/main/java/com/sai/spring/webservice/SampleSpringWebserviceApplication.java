4ecf8efc83a58245ddc87d062e0fd5a7985b8336
chandan bharadwaj <kolluruchandanbharadwaj@gmail.com>
2019-03-24 12:21:47 +0530

    initial commit

---
+++ src/main/java/com/sai/spring/webservice/SampleSpringWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sai.spring.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringWebserviceApplication.class, args);
+	}
+
+}
